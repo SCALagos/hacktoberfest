e0851d5235ce60817f1090b0f6172dbfe09d9bc1
Syed Darain Quadri <darain.quadri@gmail.com>
2023-10-28 20:10:38 +0530

    Added Hello World Task with Java

---
+++ scripts/hello_world_darainquadri.java
@@ -0,0 +1,13 @@
+// LANGUAGE: Java
+// ENV: OpenJDK
+// AUTHOR: Syed Darain Quadri
+// GITHUB: https://github.com/darainquadri
+
+public class hello_world_darainquadri {
+
+	public static void main(String args[]) {
+
+		System.out.println("Hello, World!");
+
+	}
+}
