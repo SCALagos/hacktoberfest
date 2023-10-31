5e1d055d44906fa38d805deeed30a94648fd7a51
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-10-31 22:48:32 +0530

    hello world code in cpp

---
+++ scripts/hello_world_japneetbhatia.cpp
@@ -0,0 +1,10 @@
+// language : Java
+// ENV : GCC
+// Author : Japneet Kaur
+// Github: https://github.com/japneetbhatia
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
